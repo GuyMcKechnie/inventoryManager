8a2f3a0c7a8c1279d818d6b58ae6d9b770d7a410
Guy McKechnie <mckechnieguy.mg@gmail.com>
2025-05-22 18:46:01 +0200

    refactor: clean up header and search bar components for improved readability

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -6,17 +6,13 @@ function Header() {
   return (
     <header className="flex h-[6vh] w-full items-center justify-between border border-gray-800 bg-gray-900 px-4 py-2">
       <div className="flex items-center justify-center gap-2">
-        <img src="../../icon.svg" className="size-8"/>
+        <img src="../../icon.svg" className="size-8" />
         <Typography variant="h5" className="font-times !font-bold">
           Lidio Stock
         </Typography>
       </div>
       <Searchbar />
       <div className="flex items-center justify-center gap-4">
-        <ToggleButton value="toggle1" component="button">
-          Toggle
-        </ToggleButton>
-
         <Avatar />
       </div>
     </header>
--- client/src/components/search-bar.tsx
+++ client/src/components/search-bar.tsx
@@ -1,21 +1,17 @@
-import { Search } from "@mui/icons-material";
+import { Search } from "lucide-react";
 
 function Searchbar() {
   return (
-    <form id="search-bar">
-      <div className="flex">
-        <div className="flex items-center justify-between rounded-lg bg-gray-800">
-          <div className="pointer-events-none flex items-center px-2">
-            <Search />
-          </div>
-          <input
-            type="search"
-            id="default-search"
-            className="rounded-lg border-none bg-transparent"
-            placeholder="Search..."
-            required
-          />
-        </div>
+    <form id="search-bar" className="w-full max-w-sm">
+      <div className="flex w-full items-center justify-start rounded-lg bg-gray-800 px-2 ring-blue-500 hover:ring-1 focus:outline-1">
+        <Search className="pointer-events-none flex size-10 items-center px-2" />
+        <input
+          type="search"
+          id="default-search"
+          className="w-full border-none bg-transparent px-2 py-0.5 hover:cursor-text focus:border-b-2 focus:border-b-blue-500 focus:ring-0 focus:outline-none"
+          placeholder="Search..."
+          required
+        />
       </div>
     </form>
   );
