be40320b8565b019c4f54887eac7a855cfc0cb93
Guy McKechnie <mckechnieguy.mg@gmail.com>
2025-05-31 18:47:19 +0200

    feat(sales): add loading state and loader component to Sales page

--- client/src/features/sales/components/kanban.tsx
+++ client/src/features/sales/components/kanban.tsx
@@ -9,12 +9,14 @@ import {
 import "@caldwell619/react-kanban/dist/styles.css";
 import { Typography } from "@mui/material";
 import { Plus } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Order, updateOrderStatus } from "../api/sales";
 import { createKanban } from "../helper/create-kanban";
 
 function Kanban({ orders }: { orders: Order[] }) {
   const [board, setBoard] = useState<KanbanBoard<Card>>(createKanban(orders));
+  const [loading, setLoading] = useState(true);
+  // Kanban Cards
   const handleCardMove: OnDragEndNotification<Card> = (
     _card,
     source,
@@ -25,6 +27,7 @@ function Kanban({ orders }: { orders: Order[] }) {
     });
     updateOrderStatus(_card.orderUUID, destination?.toColumnId);
   };
+
   return (
     <div className="flex h-full flex-col gap-6 rounded-lg border border-gray-800 bg-gray-900 p-6">
       <div className="flex items-center justify-between">
